cc6c43a4f150097f8d34e1339d9c3e61be825eb1
Patrick Kurth <p.kurth@posteo.de>
2023-05-31 12:22:04 +0200

    added section comments

--- cmdoptions.h
+++ cmdoptions.h
@@ -12,29 +12,36 @@
 
 struct cmdoptions;
 
+/* initialization and clean-up */
 struct cmdoptions* cmdoptions_create(void);
 void cmdoptions_enable_narrow_mode(struct cmdoptions* options);
 void cmdoptions_disable_narrow_mode(struct cmdoptions* options);
 void cmdoptions_destroy(struct cmdoptions* options);
 void cmdoptions_exit(struct cmdoptions* options, int exitcode);
 
+/* parsing */
 int cmdoptions_parse(struct cmdoptions* options, int argc, const char* const * argv);
 
+/* defining options and sections */
 int cmdoptions_add_section(struct cmdoptions* options, const char* section);
 int cmdoptions_add_option(struct cmdoptions* options, char short_identifier, const char* long_identifier, int numargs, const char* help);
 int cmdoptions_add_option_default(struct cmdoptions* options, char short_identifier, const char* long_identifier, int numargs, const char* default_arg, const char* help);
 int cmdoptions_add_alias(struct cmdoptions* options, const char* long_alias_identifier, char short_identifier, const char* long_identifier, const char* help);
 
+/* help message content */
 void cmdoptions_prepend_help_message(struct cmdoptions* options, const char* msg);
 void cmdoptions_append_help_message(struct cmdoptions* options, const char* msg);
 
+/* help display */
 void cmdoptions_help(const struct cmdoptions* options);
 void cmdoptions_export_manpage(const struct cmdoptions* options);
 
+/* query option info */
 int cmdoptions_no_args_given(const struct cmdoptions* options);
 int cmdoptions_was_provided_short(struct cmdoptions* options, char short_identifier);
 int cmdoptions_was_provided_long(struct cmdoptions* options, const char* long_identifier);
 
+/* query option content */
 const void* cmdoptions_get_argument_short(struct cmdoptions* options, char short_identifier);
 const void* cmdoptions_get_argument_long(struct cmdoptions* options, const char* long_identifier);
 const char** cmdoptions_get_positional_parameters(struct cmdoptions* options);
